fac87428705b79bdeeaf0d9292a2b2bad1ab2ffe
m0ryz <161962722+m0ryz@users.noreply.github.com>
2024-03-02 22:17:13 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const mineflayer = require("mineflayer");
+
+ for (let i = 0; i < numBots; i++) {
+    botConfigs.push({
+      host: "ip"
+      port: "port",
+      version: "1.16.5",
+      username: ярик лох${i + 1}
+    });
+  }
+
+let { username } = bot.nearestEntity(({ type }) => type === 'player')
+
+bot.chat('username лох!')

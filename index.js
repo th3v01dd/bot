3486a6b59c73c60b2e3fcc864259269c3784b471
m0ryz <161962722+m0ryz@users.noreply.github.com>
2024-04-22 01:09:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const mineflayer = require("mineflayer");
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6914028704:AAGQPWD03MX14GdqcZc82hTpqF0actaXvFo';
+const bot = new TelegramBot(token, { polling: true });
+let balanceMap = {};
 
- for (let i = 0; i < numBots; i++) {
-    botConfigs.push({
-      host: "localhost"
-      port: "53187",
-      version: "1.16.5",
-      username: ярик лох${i + 1}
-    });
-  }
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    if (!balanceMap[chatId]) {
+        balanceMap[chatId] = 0;
+    }
+    const opts = {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'Mine HFAS', callback_data: 'mine_hfas' }],
+                [{ text: 'Balance', callback_data: 'view_balance' }]
+            ]
+        }
+    };
+    bot.sendMessage(chatId, 'Choose an option:', opts);
+});
 
-  const bots = [];
-
-  botConfigs.forEach((config, index) => {
-    setTimeout(() => {
-      const bot = mineflayer.createBot(config)};
+bot.on('callback_query', (callbackQuery) => {
+    const chatId = callbackQuery.message.chat.id;
     
-      bot.on("login", () => {
-        console.log(Bot${index + 1} logged in)};
-      });
-
- bots.push(bot);
-    }, 5000 * index); 
-     
-
-let { username } = bot.nearestEntity(({ type }) => type === 'player')
-
-bot.chat('username лох!')
-createBots(5);
+    if (callbackQuery.data === 'mine_hfas') {
+        const numHfas = Math.floor(Math.random() * 5) + 1;
+        balanceMap[chatId] += numHfas;
+        bot.editMessageText(`HFAS mined: ${numHfas}\nBalance: ${balanceMap[chatId]} HFAS`, { chat_id: chatId, message_id: callbackQuery.message.message_id, reply_markup: {
+            inline_keyboard: [
+                [{ text: 'Mine HFAS', callback_data: 'mine_hfas' }],
+                [{ text: 'Balance', callback_data: 'view_balance' }]
+            ]
+        }});
+    } else if (callbackQuery.data === 'view_balance') {
+        bot.editMessageText(`Balance: ${balanceMap[chatId]} HFAS`, { chat_id: chatId, message_id: callbackQuery.message.message_id, reply_markup: {
+            inline_keyboard: [
+                [{ text: 'Mine HFAS', callback_data: 'mine_hfas' }],
+                [{ text: 'Balance', callback_data: 'view_balance' }]
+            ]
+        }});
+    }
+});
